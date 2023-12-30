a0562479b271de904686516fe590a13f84990538
Satish9047 <satishprajapati930@gmail.com>
2023-12-30 22:52:21 +0545

    initilizing todo api

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, ():void=>{
+    console.log(`Server running on port ${port}`);
+});
